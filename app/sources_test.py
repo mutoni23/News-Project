fbd115cc406d39916f40e37e3c66c60b22142b78
mutoni vivine <mutoniv5@gmail.com>
2020-11-22 19:03:22 +0200

    added unit testing

--- app/sources_test.py
+++ app/sources_test.py
@@ -9,8 +9,8 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source(1231,'my web','my description','https://www.osdns.com','category','en','rwanda')
+        self.new_source = Source('1id231','my web','my description','https://www.osdns.com','category','en','rwanda')
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
-if __name__ == '__main__':source
+if __name__ == '__main__':
     unittest.main()
--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,3 @@
 NEWS_API_KEY = '27debc9a3caf43b98a16e58bbcb1607b'
 
 NEWS_API_BASE_URL = 'https://newsapi.org/v2/'
-# SECRET_KEY = '1234567'
-
